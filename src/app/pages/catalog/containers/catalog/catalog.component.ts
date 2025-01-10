aa5a043908da7e56ae290d881d6b5d2f751a8b2b
PAVEL KHALUNIN <wol1414@gmail.com>
2025-01-10 12:08:43 +0100

    refactor: implements OnInit

--- src/app/pages/catalog/containers/catalog/catalog.component.ts
+++ src/app/pages/catalog/containers/catalog/catalog.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, type OnInit } from '@angular/core';
 import { ItemsListComponent } from '../../components/items-list/items-list.component';
 import { RequestService } from '../../services/request.service';
 import { ItemInterface } from '../../models/item.model';
@@ -10,12 +10,12 @@ import { ItemInterface } from '../../models/item.model';
   styleUrl: './catalog.component.scss',
   standalone: true,
 })
-export class CatalogComponent {
-  requestService = inject(RequestService);
+export class CatalogComponent implements OnInit {
+  #requestService = inject(RequestService);
 
-  items = this.requestService.items;
+  items = this.#requestService.items;
 
-  constructor() {
-    this.requestService.loadItems();
+  async ngOnInit() {
+    await this.#requestService.loadItems();
   }
 }
